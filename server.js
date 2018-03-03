42863f05b3c2fe217cf9b3e54e3149874b954c9d
Molly Jean Bennett <mollyjeanbennett@gmail.com>
2018-03-02 18:57:25 -0800

    set up express server and hello world html

---
+++ server.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+
+if (require.main === module) {
+  app.listen(process.env.PORT || 8080, function() {
+    console.info(`App listening on ${this.address().port}`);
+  });
+}
+
+module.exports = app;
