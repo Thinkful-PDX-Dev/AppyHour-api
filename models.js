fe259aea532eeae8a0b885a3afa702bb2c0dc1e3
Donna Lambert <dlambertpdx@gmail.com>
2018-08-23 15:52:42 -0700

    update models seed-data

--- models.js
+++ models.js
@@ -6,14 +6,22 @@ mongoose.Promise = global.Promise;
 //Schema for bar data
 
 const barSchema = mongoose.Schema({
-  name: {type: String, required: true},
-  address: {type: String, required: true},
-  hours: {type: String, required: true},
-  description: {type: String}
-})
+  name: { type: String, required: true },
+  address: { type: String, required: true },
+  hours: { type: String, required: true },
+  description: { type: String },
+  neighborhood: { type: String },
+  phone: { type: String },
+  rating: { type: String },
+  review: { type: String },
+  price: { type: String },
+  link: { type: [String] },
+  img: { type: String },
+  patio: { type: Boolean }
+});
 
-barSchema.set('toObject', {
-  transform: function (doc,ret) {
+barSchema.set("toObject", {
+  transform: function(doc, ret) {
     ret.id = ret._id;
     delete ret._id;
     delete ret.__v;
@@ -26,12 +34,20 @@ barSchema.methods.serialize = function() {
     name: this.name,
     address: this.address,
     hours: this.hours,
-    description: this.description
-  }
-}
+    description: this.description,
+    priceRange: this.priceRange,
+    neighborhood: this.neighborhood,
+    rating: this.rating,
+    img: this.img,
+    links: this.links,
+    patio: this.patio,
+    review: this.review,
+    phone: this.phone
+  };
+};
 
 // const Bar = mongoose.model("Bar", barSchema);
 
 // module.exports = {Bar}
 
-module.exports = mongoose.model('Bar', barSchema)
+module.exports = mongoose.model("Bar", barSchema);
